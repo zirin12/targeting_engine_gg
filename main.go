958b25ce3f415e69cdee64ef30c7ec01d8b62d71
zirin12 <rpillai1998@gmail.com>
2025-07-13 18:07:25 +0530

    - Validate the three query params - Mock json response of a hardcoded campaign slice - Added TODOs for pending implementation of in   memory data structure and targeting rule match

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,8 +11,42 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "OK")
 }
 
+func deliveryHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET method allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Extract query parameters
+	app := r.URL.Query().Get("app")
+	country := r.URL.Query().Get("country")
+	os := r.URL.Query().Get("os")
+
+	// Check if any of the params are empty
+	if app == "" || country == "" || os == "" {
+		http.Error(w, "missing required query params", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: All campaigns need to be in a slice of structs in memory fetched from the DB
+	// Mocked response - just to check the response
+	campaigns := []map[string]string{
+		{
+			"cid": "duolingo",
+			"img": "https://somelink2",
+			"cta": "Install",
+		},
+	}
+
+	// TODO: need to match the campaign rules against the input params to know which should be considered
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(campaigns)
+}
+
 func main() {
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/v1/delivery", deliveryHandler)
 
 	port := ":8080"
 	log.Printf("Starting delivery service on port %s ...", port)
